c7460fb1f5bf58c5e4e7a6ee312d7dd3fe16eb9f
adhitya <adhitya092@gmail.com>
2025-04-09 08:39:28 +0700

    commit Tugas pekan 3 membuat web statis menggunakan laravel 12

---
+++ resources/views/register.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Form Sign Up</title>
+</head>
+<body>
+    <h1>Buat Account Baru</h1>
+
+    <h3>Sign Up Form</h3>
+    
+    <form action="/welcome" method="POST">
+    @csrf
+        <label >First Name:</label><br>
+        <input type="text"  name="first_name"><br><br>
+
+        <label >Last Name:</label><br>
+        <input type="text"  name="last_name"><br><br>
+
+        <label>Gender:</label><br>
+        <input type="radio" name="gender" value="Male">
+        <label>Man</label><br>
+
+        <input type="radio"  name="gender" value="Female">
+        <label >Woman</label><br>
+
+        <input type="radio" name="gender" value="Other">
+        <label>Other</label><br><br>
+
+        <label for="nationality">Nationality:</label><br>
+        <select id="nationality" name="nationality">
+            <option value="indonesia">Indonesia</option>
+            <option value="singapore">Singapore</option>
+            <option value="malaysia">Malaysia</option>
+            <option value="australia">Australia</option>
+        </select><br><br>
+
+        <label>Language Spoken:</label><br>
+        <input type="checkbox" id="bahasa" name="language[]" value="Bahasa Indonesia">
+        <label for="bahasa">Bahasa Indonesia</label><br>
+
+        <input type="checkbox" id="english" name="language[]" value="English">
+        <label for="english">English</label><br>
+
+        <input type="checkbox" id="arabic" name="language[]" value="Arabic">
+        <label for="arabic">Arabic</label><br>
+
+        <input type="checkbox" id="japanese" name="language[]" value="Japanese">
+        <label for="japanese">Japanese</label><br><br>
+
+        <label for="bio">Bio:</label><br>
+        <textarea id="bio" name="bio" rows="5" cols="30"></textarea><br><br>
+
+        <input type="submit" value="Sign Up">
+    </form>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Selamat Datang {{ $name }}</h1>
+    <p><b>Terima kasih telah bergabung di Sanberbook. Social Media kita bersama!</b></p>
+</body>
+</html>
